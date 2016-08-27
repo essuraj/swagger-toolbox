05a47bcc2c560579bda9db1abc58b492907cdb08
Suraj <essuraj@live.in>
2016-08-27 22:10:55 +0530

    conevrt to yaml completed

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,46 @@
-var editor = CodeMirror.fromTextArea(document.getElementById("myTextarea"), {
+var jsonEditor = CodeMirror.fromTextArea(document.getElementById("myTextarea"), {
     lineNumbers: true,
     mode: "javascript"
 });
 
-function tryParseJSON(jsonString) {
-    try {
-        var o = JSON.parse(jsonString);
-        if (o && typeof o === "object") {
-            return o;
-        }
-    } catch (e) {}
-
-    return false;
-};
-var editor2 = CodeMirror.fromTextArea(document.getElementById("myTextarea2"), {
-    lineNumbers: true,
+var yamlEditor = CodeMirror.fromTextArea(document.getElementById("myTextarea2"), {
     mode: "yaml"
 });
+
+class MainController {
+
+    processJSON() {
+        var jsonString = jsonEditor.getValue();
+        console.info(jsonString);
+        if (this.tryParseJSON(jsonString) === true) {
+            console.info(true);
+            this.statusMessage = "Valid json ✔";
+            this.status = "success";
+            var json = JSON.parse(jsonString);
+            var x = stringify(json);
+            console.log(x);
+            yamlEditor.setValue(x);
+
+        } else {
+            this.statusMessage = "Error with JSON";
+            this.status = "error";
+        }
+    }
+    tryParseJSON(jsonString) {
+        try {
+            var o = JSON.parse(jsonString);
+            if (o && typeof o === "object") {
+                return true;
+            }
+        } catch (e) {
+            console.error(e, jsonString);
+            return false;
+        }
+
+        return false;
+    };
+}
+
+angular
+    .module("swagger-toolbox", [])
+    .controller('mainController', MainController);
